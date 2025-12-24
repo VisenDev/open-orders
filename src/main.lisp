90dc01685a684302241b4ca3ad18eecdaef36608
Robert Burnett <dr.slate2@gmail.com>
2025-12-23 21:07:35 -0600

    create a slick mechanism for defining tables that creates the sql code and the clos class

--- src/main.lisp
+++ src/main.lisp
@@ -20,6 +20,38 @@
      ,@body)
   )
 
+(defparameter *tables* nil "list of the sql tables that have been defined in this program")
+
+(defun valid-sql-identifer-p (symbol)
+  (let ((str (symbol-name symbol)))
+    (loop :for ch :across str
+          :always (or (eq ch #\_) (alphanumericp ch)))))
+
+(defmacro deftable (name &rest fields)
+  (unless (valid-sql-identifer-p name)
+    (error "Invalid sql table name: ~a" name))
+  (let ((clos-fields (loop :for field :in fields
+                           :for field-name = (first field)
+                           :for type = (second (getf (cdr field) :type t))
+                           :collect (list field-name
+                                          :accessor field-name
+                                          :type type
+                                          :initarg (intern (symbol-name field-name) 'keyword))
+                           :do (unless (valid-sql-identifer-p field-name)
+                                 (error "Invalid sql field name: '~a~%" field-name))
+
+                           ))
+        (varname (intern (format nil "*SQL-CREATE-TABLE-~a*" (symbol-name name))))
+        (classname (intern (format nil "~a-ROW" (symbol-name name)))))
+    `(progn
+       (push ,(list 'quote name) *tables*)
+       (defclass ,classname ()
+         ,clos-fields)
+       (defparameter ,varname (sxql:create-table ,name ,fields))
+       ))
+  )
+
+
 ;;; IMPL
 (defparameter *database-path* "database.sqlite3")
 (defvar *database* nil)
@@ -34,26 +66,26 @@
           (multiple-value-bind (sql params) (sxql:yield ,@body)
             (dbi:execute (dbi:prepare *database* sql) params))))
 
+(deftable customers
+  (id :type 'integer :primary-key t :auto-increment t)
+  (name :type 'text :not-null t)
+  (primary_contact_name :type 'text)
+  (primary_contact_email :type 'text))
+
+(deftable orders
+  (id :type 'integer :primary-key t :auto-increment t)
+  (purchase_order_number :type 'text)
+  (customer_id :type 'integer)          ;foreign key references customers
+  (part_number :type 'text)
+  (quantity :type 'integer)
+  (due_date :type 'text)
+  )
+
 (defun create-tables ()
   (do-sql (sxql:drop-table :customers :if-exists t))
   (do-sql (sxql:drop-table :orders  :if-exists t))
-  (do-sql
-    (sxql:create-table customers
-        ((id :type 'integer
-             :primary-key t
-             :auto-increment t)
-         (name :type 'text
-               :not-null t))))
-  (do-sql
-    (sxql:create-table orders
-        ((id :type 'integer
-             :primary-key t
-             :auto-increment t)
-         (purchase_order_number :type 'text)
-         (customer_id :type 'integer)   ;foreign key references customers
-         (part_number :type 'text)
-         (quantity :type 'integer)
-         (due_date :type 'text))))
+  (do-sql *sql-create-table-customers*)
+  (do-sql *sql-create-table-orders*)
   )
 
 (defun list-open-orders (obj)
@@ -93,7 +125,7 @@
   (insert-random-order)
   (setf (title (html-document body)) "Open Orders")
   (let* ((menu (create-div body))
-         (contents (create-div body :style "display: flex; flex-direction: row;"))
+         (contents (create-div body :style "display: flex; flex-direction: row"))
          (display-panel (create-div contents))
          (edit-panel (create-div contents))
          (home-tab (create-button menu :content "Home" :style "font-weight:bold;"))
@@ -106,6 +138,7 @@
                     (insert-random-order)
                     (destroy-children display-panel)
                     (list-open-orders display-panel)))
+    (create-p edit-panel :content *tables*)
     (list-open-orders display-panel)
     (set-on-click home-tab (callback (obj) (create-section display-panel :p :content "clicked home tab!")))
     (set-on-click open-orders-tab (callback (obj) (create-section display-panel :p :content "licked open-orders-tab")))
