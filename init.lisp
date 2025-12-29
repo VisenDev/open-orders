f6cd48cbf79f6113e6a563aeb7e44c6b0ba577c6
Robert Burnett <dr.slate2@gmail.com>
2025-12-28 20:54:58 -0600

    update files that i forgot to commit

--- init.lisp
+++ init.lisp
@@ -1,8 +1,15 @@
-(unless (find-package "asdf")
+(unless (find-package 'asdf)
   (format t ";;; Loading asdf...~%")
   (load "tooling/asdf/build/asdf.lisp")
   )
 
+#+sbcl (declaim (sb-ext:muffle-conditions sb-ext:compiler-note))
+#+sbcl (declaim (sb-ext:muffle-conditions cl:warning))
+
 (asdf:initialize-source-registry `(:source-registry (:tree ,(uiop:getcwd)) :ignore-inherited-configuration))
 (asdf:load-system "clog")
+
+#+sbcl(declaim (sb-ext:unmuffle-conditions sb-ext:compiler-note))
+#+sbcl(declaim (sb-ext:unmuffle-conditions cl:warning))
+
 (asdf:load-system "cl-db")
