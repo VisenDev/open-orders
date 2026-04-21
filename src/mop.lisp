78d6c375886523543d4e8594e917f8d33cca8bae
Robert Burnett <dr.slate2@gmail.com>
2026-04-21 16:57:32 -0500

    more work on mop

--- src/mop.lisp
+++ src/mop.lisp
@@ -5,13 +5,23 @@
                     (#:mop #:closer-mop)))
 (in-package #:open-orders.mop)
 
+(defun lisp-identifier->sql-identifier (ident)
+  (intern (map 'string (lambda (ch)
+                    (if (alphanumericp ch) ch #\_))
+                  (symbol-name ident))))
+
 (defclass sql-table (standard-class)
-  ((active-tables :initform (make-hash-table))))
+  ((sql-name :accessor sql-name)))
+
+(defmethod mop:finalize-inheritance :after ((class sql-table))
+  (setf (sql-name class)
+        (lisp-identifier->sql-identifier (class-name class))))
 
 (defclass sql-table-effective-slot-definition (mop:standard-effective-slot-definition)
   ((primary-key :accessor primary-key :initform nil)
    (references :accessor references :initform nil)
-   (autoincrement :accessor autoincrement :initform nil)))
+   (autoincrement :accessor autoincrement :initform nil)
+   (sql-name :accessor sql-name :initform nil)))
 
 (defclass sql-table-direct-slot-definition (mop:standard-direct-slot-definition)
   ((primary-key :accessor primary-key :initform nil
@@ -40,17 +50,26 @@
           (some #'references direct-slots))
     (setf (autoincrement eslot)
           (some #'autoincrement direct-slots))
+    (setf (sql-name eslot)
+          (lisp-identifier->sql-identifier name))
     eslot))
 
-;; (defmethod mop:slot-definition-allocation ((slotd sql-table-effective-slot-definition))
-;;   'instance)
+(defmethod mop:slot-definition-allocation ((slotd sql-table-effective-slot-definition))
+  (call-next-method))
 
-;; (defmethod mop:slot-definition-allocation ((slotd sql-table-direct-slot-definition))
-;;   'instance)
+(defmethod mop:slot-definition-allocation ((slotd sql-table-direct-slot-definition))
+  (call-next-method))
 
 (defclass standard-sql-table ()
   ((id :accessor id :primary-key t :auto-increment t :initform nil :initarg :id))
   (:metaclass sql-table))
+
+
+
+(defgeneric create-table (class database))
+(defmethod create-table ((class sql-table) database)
+  (format nil "CREATE TABLE ~a" (sql-name class)))
+
 
 (defclass person (standard-sql-table)
   ((first-name :accessor first-name)
