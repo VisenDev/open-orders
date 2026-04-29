c8f5699a01d47ae0605fd6682b834a723244fb22
Robert Burnett <dr.slate2@gmail.com>
2026-04-28 20:21:09 -0500

    fix html metaclass

--- src/html-metaclass.lisp
+++ src/html-metaclass.lisp
@@ -1,4 +1,4 @@
-(uiop:define-package #:open-orders.html-form
+(defpackage #:open-orders.html-form
     (:use #:cl)
   (:import-from #:defclass-std
                 #:defclass/std
@@ -8,7 +8,8 @@
                 #:*let)
   (:local-nicknames (#:a #:alexandria)
                     (#:mop #:closer-mop)
-                    (#:db #:open-orders.db))
+                    ;; (#:db #:open-orders.db)
+                    )
   (:shadow #:type #:min #:max))
 (in-package #:open-orders.html-form)
 
@@ -37,16 +38,16 @@
            :week
            :foreign-key-edit))
 
-(defclass html-form (db:sql-table) ())
+(defclass html-form (standard-class) ())
 
-(defclass/std html-form-effective-slot-definition (db::sql-table-effective-slot-definition)
+(defclass/std html-form-effective-slot-definition (mop:standard-effective-slot-definition)
   ((input-type :std :text)
    (name label min max pattern placeholder)))
 
-(defclass/std html-form-direct-slot-definition (db::sql-table-direct-slot-definition)
+(defclass/std html-form-direct-slot-definition (mop:standard-direct-slot-definition)
   ((input-type name label min max pattern placeholder)))
 
-(defmethod mop:validate-superclass ((class html-form) (superclass db:sql-table))
+(defmethod mop:validate-superclass ((class html-form) (superclass standard-class))
   t)
 
 (defmethod mop:effective-slot-definition-class ((class html-form) &rest initargs)
@@ -86,25 +87,29 @@
           :for label-elem = (clog:create-label form :content label)
           :for elem = (clog:create-form-element
                        form input-type)
-          :for value = nil
-          :when (eq input-type :range)
-            :do (setf value (clog:create-p form :content "50"))
-          :end
+          ;; :for value = nil
+          ;; :when (eq input-type :range)
+          ;;   :do (setf value (clog:create-p form :content "50"))
+          ;; :end
           :do (clog:create-br form)
              (clog:label-for label-elem elem)
               (let ((slot slot)
                     (elem elem)
                     (input-type input-type)
                     (instance instance)
-                    (value value))
+                    (label-elem label-elem))
                 (clog:set-on-change
                  elem
                  (fn (obj)
                    (setf (slot-value instance
                                      (mop:slot-definition-name slot))
                          (clog:value elem))
-                   (when  (eq input-type :range)
-                     (setf (clog:inner-html value) (age instance)))))))
+                   (when (eq input-type :range)
+                     (setf (clog:inner-html label-elem)
+                           (format nil "~a:  ~a"
+                                   (mop:slot-definition-name slot)
+                           (slot-value instance
+                                     (mop:slot-definition-name slot)))))))))
     ;; (clog:set-on-mouse-click form (fn (obj event) (format t "~a"
     ;;                                                 (age instance)
     ;;                                                 )))
@@ -120,9 +125,12 @@
   (:metaclass html-form))
 
 (defun test ()
-  (clog:initialize (lambda (body)
-                     (let ((p (make-instance 'test-person :age 20)))
-                       (create-form-from-object body p))))
+  (clog:initialize
+   (lambda (body)
+     (clog:load-css (clog:html-document body)
+                    "https://cdn.jsdelivr.net/npm/@picocss/pico@2.1.1/css/pico.min.css")
+     (let ((p (make-instance 'test-person :age 20)))
+       (create-form-from-object (clog:create-div body :class "container") p))))
   (clog:open-browser))
 
 
