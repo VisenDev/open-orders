e7ec8a700869997290d080a3f722882203bdc04f
Robert Burnett <dr.slate2@gmail.com>
2026-09-23 11:43:20 -0500

    a lot of bug fixes and robustness improvements for the database

--- modules/main/inventory.lisp
+++ modules/main/inventory.lisp
@@ -2,17 +2,18 @@
   (:use #:cl #:open-orders.pagen #:open-orders.tables))
 (in-package #:open-orders.main.inventory)
 
-(defvar *tables* (make-hash-table :test 'equal))
-(defvar *file-extension* "sexp")
-(defvar *database-path* "database/")
-
-(defstruct param
-  (name nil :type symbol)
-  (type t)
-  (kind nil :type (member :required :optional :key :rest :aux))
-  initform)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *tables* (make-hash-table :test 'equal))
+  (defvar *file-extension* "sexp")
+  (defvar *database-path* "database/"))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
+  (defstruct param
+    (name nil :type symbol)
+    (type t)
+    (kind nil :type (member :required :optional :key :rest :aux))
+    initform)
+  
   (defun parse-typed-lambda-list (params)
     (let ((state :required)
           (results nil))
@@ -43,7 +44,7 @@
                                                :initform (second val))
                                    results))
                             ((listp val)
-                             (push (make-param :name (first (first val))
+                             (push (make-param :name (first val)
                                                :kind state
                                                :initform (second val))
                                    results))
@@ -123,6 +124,26 @@
                                  params))
               (the ,return-type (progn ,@body))))))
 
+
+
+
+#+clisp
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require "syscalls"))
+
+;; Code ported from uiop, because rename can't replace a file on ecl
+(fn (rename-file-overwriting-target t) ((source pathname) (target pathname))
+  #+clisp (posix:copy-file source target :method :rename)
+  #+(and sbcl win32)
+  (when target (handler-case (delete-file target) (file-error () nil))) ;; not atomic
+  #-clisp
+  (rename-file source target
+               #+(or clasp clozure ecl) :if-exists
+               #+clozure :rename-and-delete #+(or clasp ecl) t))
+
+
+
+;;;; DATABASE IMPLEMENTATION
 (fn (valid-table-designator-char-p boolean) ((ch character))
   (or (alphanumericp ch) (char= #\_ ch) (char= #\- ch)))
 
@@ -136,7 +157,7 @@
     ((database-path (or string pathname))
      (table-name table-designator))
   (unless (string= (directory-namestring database-path)
-                   database-path)
+                   (namestring database-path))
     (error "database-path must end with a /"))
   (ensure-directories-exist
    (merge-pathnames (string-downcase
@@ -157,11 +178,12 @@
     (when (probe-file filename)
       (with-open-file (fp filename)
         (let ((cl:*read-eval* nil))
-          (read fp))))))
+          (unless (zerop (file-length fp))
+            (read fp)))))))
 
-(fn (find-free-id integer) ((database-path (or pathname string))
+(fn (table-find-free-id integer) ((database-path (or pathname string))
                             (table-name table-designator) &optional
-                            ((depth integer) 0))
+                            ((retries integer) 0))
 
   (let* ((paths (directory
                  (merge-pathnames
@@ -184,89 +206,119 @@
                 :if-does-not-exist :create)
           new-id)
       (file-error (err)
-        (if (< depth 10)
-            (find-free-id database-path table-name (1+ depth))
+        (if (< retries 3)
+            (table-find-free-id database-path table-name (1+ retries))
             (error err))))))
 
 (fn (table-set t) ((database-path (or string pathname))
                    (table-value t)
-                   (id integer))
+                   (id integer)
+                   &optional
+                   ((retries integer) 0))
   (let* ((table-name (class-name (class-of table-value)))
          (directory (truename (table-directory-get database-path table-name)))
          (tmp-pathname (merge-pathnames
-                        (format nil "temp-~a.~a" (random 100000) *file-extension*)
+                        (format nil "~a-~a.tmp" (random 10000000) id)
                         directory))
          (output-pathname (merge-pathnames
                            (format nil "~a.~a" id *file-extension*)
                            directory)))
-    (with-open-file (fp tmp-pathname :direction :output :if-exists :error)
-      (format fp "~S" table-value))
-    (rename-file tmp-pathname output-pathname)))
+    (handler-case
+        (with-open-file (fp tmp-pathname :direction :output :if-exists :error)
+          (format fp "~S" table-value))
+      (file-error (e)
+        (if (> retries 3)
+            (error e)
+            ;; else retry
+            (progn
+              (return-from table-set
+                (table-set database-path table-value id (1+ retries)))))))
 
-(fn (table-get-all list) ((database-path (or string pathname))
-                          (table-name table-designator))
+    ;; rename tmp file on success
+    (handler-case 
+        (rename-file-overwriting-target tmp-pathname output-pathname)
+      (file-error (e)
+
+        ;; clean up tempfile if rename fails and output pathname still exists
+        (when (and (probe-file tmp-pathname) (probe-file output-pathname))
+          (ignore-errors (delete-file tmp-pathname)))
+        (error e)))))
+
+(fn (table-get-all vector) ((database-path (or string pathname))
+                            (table-name table-designator)
+                            &optional
+                            ((output-buffer vector)
+                             (make-array 7 :adjustable t :fill-pointer 0)))
+  (assert (adjustable-array-p output-buffer))
+  (assert (array-has-fill-pointer-p output-buffer))
+  (setf (fill-pointer output-buffer) 0)
   (loop :for path :in (directory
                        (merge-pathnames
                         (make-pathname :name :wild :type *file-extension*)
                         (table-directory-get database-path table-name)))
-        :collect (with-open-file (fp path)
-                   (let ((cl:*read-eval* nil))
-                     (read fp)))))
+        :do
+           (with-open-file (fp path)
+             (unless (zerop (file-length fp))
+               (let ((cl:*read-eval* nil))
+                 (vector-push-extend (read fp) output-buffer))))
+        :finally (return output-buffer)))
 
-(defstruct field
-  (name nil :type symbol)
-  accessor
-  type
-  compare-function
-  initform
-  references)
-(defstruct table
-  (name nil :type table-designator)
-  id-accessor
-  (fields nil :type list)
-  (conc-name nil :type symbol))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defstruct (field
+              (:constructor field
+                  (name &key type compare-function initform references)) )
+    (name nil :type symbol)
+    accessor
+    (type t)
+    compare-function
+    initform
+    references)
+  (defstruct table
+    (name nil :type table-designator)
+    id-accessor
+    (fields nil :type list)
+    (conc-name nil :type symbol)))
 
 ;;; For slime completion of arguments
 ;;; Not actually used for anything
-(defmacro field (name &key type compare-function initform references)
-  (declare (ignore name type compare-function initform references)))
+;; (defmacro field (name &key type compare-function initform references)
+;;   (declare (ignore name type compare-function initform references)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun symbolicate (&rest things)
     (intern (string-upcase
              (apply #'concatenate 'string
                     (mapcar (lambda (thing) (format nil "~a" thing)) things)))))
-  (defun parse-table-definition (name fields conc-name)
+  (defun parse-table-definition (name field-forms conc-name)
     (let ((conc-name (or conc-name (symbolicate name '-))))
       (make-table :name name
                   :conc-name conc-name
                   :id-accessor (symbolicate conc-name 'id)
                   :fields (cons
-                           (make-field :name 'id
-                                       :accessor (symbolicate conc-name 'id)
-                                       :type '(or null integer))
+                           (let ((id (field 'id :type '(or null integer))))
+                             (setf (field-accessor id)
+                                   (symbolicate conc-name 'id))
+                             id)
                            (mapcar
-                            (lambda (field)
-                              (destructuring-bind
-                                  (_ field-name &key type compare-function
-                                                  initform references)
-                                  field
-                                (declare (ignore _))
-
-                                (make-field
-                                 :name field-name
-                                 :type (or type t)
-                                 :accessor (symbolicate conc-name field-name)
-                                 :initform initform
-                                 :compare-function compare-function
-                                 :references references)))
-                            fields))))))
+                            (lambda (field-form)
+                              (assert (eq 'field (first field-form)))
+                              (let ((field (apply #'field (cdr field-form))))
+                                (unless (field-accessor field)
+                                  (setf (field-accessor field)
+                                        (symbolicate conc-name (field-name field))))
+                                field))
+                            field-forms))))))
 
 
 (defmacro define-table (name fields &key conc-name)
   (let ((def (parse-table-definition name fields conc-name)))
-    (setf (gethash (symbol-name name) *tables*) def)
     `(progn
+       (eval-when (:compile-toplevel :load-toplevel :execute)
+         (setf (gethash ,(symbol-name name) *tables*)
+               (parse-table-definition
+                ',name
+                ',fields
+                ',conc-name)))
        (defstruct (,(table-name def) (:conc-name ,(table-conc-name def)))
          ,@(mapcar (lambda (f)
                      (list (field-name f) (field-initform f)
@@ -275,27 +327,34 @@
 
        ,(macroexpand
          `(fn (,(symbolicate 'get- (table-name def)) (or null ,(table-name def)))
-             ((id integer)
-              &optional ((database-path (or string pathname)) *database-path*))
-           (table-get database-path ',(table-name def) id)))
+              ((id integer)
+               &optional ((database-path (or string pathname)) *database-path*))
+            (table-get database-path ',(table-name def) id)))
 
        ,(macroexpand
          `(fn (,(symbolicate 'set- (table-name def)) t)
-             ((,(table-name def) ,(table-name def))
-              &optional ((database-path (or string pathname)) *database-path*))
-           (let ((id (,(table-id-accessor def) ,(table-name def))))
-             (declare (type integer id))
-             (unless id
-               (setf id (table-find-free-id database-path ',(table-name def)))
-               (setf (,(table-id-accessor def) ,(table-name def)) id))
-             (table-set database-path ,(table-name def) id))))
+              ((,(table-name def) ,(table-name def))
+               &optional ((database-path (or string pathname)) *database-path*))
+            (let ((id (,(table-id-accessor def) ,(table-name def))))
+              (declare (type (or integer null) id))
+              (unless id
+                (setf id (table-find-free-id database-path ',(table-name def)))
+                (setf (,(table-id-accessor def) ,(table-name def)) id))
+              (table-set database-path ,(table-name def) id))))
 
        ,(macroexpand
-         `(fn (,(symbolicate 'get-every- (table-name def)) list)
-             (&optional ((database-path (or string pathname)) *database-path*))
-           (table-get-all database-path ',(table-name def)))))))
+         `(fn (,(symbolicate 'get-every- (table-name def))
+               (vector ,(table-name def) *))
+              (&optional ((database-path (or string pathname)) *database-path*))
+            (table-get-all database-path ',(table-name def)))))))
+
+(defmacro with-database (path &body body)
+  `(let ((*database-path* ,path))
+     ,@body))
 
 
+
+;;;; TESTS
 (define-table person
     ((field first-name :type string :initform "")
      (field last-name :type string :initform "")
